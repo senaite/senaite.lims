696da530410885f650502c5250b40a8d9bde89fc
Ramon Bartl <rb@ridingbytes.com>
2018-12-21 11:53:46 +0100

    Integration for senaite.core 1.3 (#90)
    * Removed bika_listing bootstrap integrations
    
    * Removed listing table reference
    
    * Added Git Ignores
    
    * Removed version pin for setuptools
    
    * Updated buildout to Plone 4.3.18
    
    * Pinned setuptools to 33.1.1
    
    * Fixed JS order in registry
    
    * Updated lxml to 4.2.1
    
    Signed-off-by: Ramon Bartl <rb@ridingbytes.com>
    
    * Updated buildout
    
    Signed-off-by: Ramon Bartl <rb@ridingbytes.com>
    
    * Travis install parameters changed
    
    Signed-off-by: Ramon Bartl <rb@ridingbytes.com>
    
    * password in buildout changed
    
    Signed-off-by: Ramon Bartl <rb@ridingbytes.com>
    
    * unpin setuptools and updated travis.yml
    
    Signed-off-by: Ramon Bartl <rb@ridingbytes.com>
    
    * Removed resultsinterpretation stylings
    
    * Added senaite.lims to eggs section
    
    * Wrap portlets into Panels
    
    * Changelog updated

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "senaite.core",
+        "senaite.core>=1.3.0",
         "senaite.api",
         "senaite.jsonapi",
         "setuptools",

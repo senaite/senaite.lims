26bf9b1fda96f9bcdcb5a20e4200bc80c42bd701
Ramon Bartl <rb@ridingbytes.com>
2019-03-28 17:20:35 +0100

    Removed dependency

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         "test": [
             "Products.PloneTestCase",
             "Products.SecureMailHost",
-            "plone.app.robotframework",
             "plone.app.testing",
             "unittest2",
         ]

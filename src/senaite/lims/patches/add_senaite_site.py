2d1612c9cf9f253281e10174c375aae99a33be6b
Ramon Bartl <rb@ridingbytes.com>
2019-03-28 17:38:38 +0100

    Better styles and auto-redirect

--- src/senaite/lims/patches/add_senaite_site.py
+++ src/senaite/lims/patches/add_senaite_site.py
@@ -2,12 +2,17 @@ from OFS.ObjectManager import ObjectManager
 
 ADD_SENAITE_SITE = """
 <dtml-if expr="_.len(this().getPhysicalPath()) == 1 and len(objectValues('Plone Site')) == 0">
+  <script type="text/javascript">
+    var url = location.href.replace(location.pathname, "/@@senaite-addsite?site_id=senaite")
+    location.href = url;
+  </script>
 </dtml-if>
 <dtml-if expr="_.len(this().getPhysicalPath()) == 1 and len(objectValues('Plone Site')) > 0">
   <!-- Add Plone site action-->
   <style type="text/css">
-   .form { margin: 1em auto; text }
-   .button { padding: 6px 12px; background-color: #337ab7; color: #fff; }
+   .form { margin: 1em auto; text-align:center; }
+   .button {padding:1em 3em;background-color:#337ab7;color:#fff;border-color:#2e6da4;cursor:pointer;font-weight:bold; }
+   input[value='Add Plone Site'] { display:none; }
   </style>
   <form class="form"
         method="get"

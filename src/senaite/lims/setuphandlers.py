400f0dce7fac770af0901c84e221256d6d91cccb
Ramon Bartl <rb@ridingbytes.com>
2022-10-04 10:21:52 +0200

    Prepare release 2.3.0 (#133)
    * Changelog updated
    
    * Pinned versions
    
    * Removed unused code

--- src/senaite/lims/setuphandlers.py
+++ src/senaite/lims/setuphandlers.py
@@ -55,20 +55,3 @@ def pre_install(portal_setup):
         portal_setup.runAllImportStepsFromProfile(profile_id)
 
     logger.info("SENAITE LIMS pre-install handler [DONE]")
-
-
-def post_install(portal_setup):
-    """Runs after the last import step of the *default* profile
-
-    This handler is registered as a *post_handler* in the generic setup profile
-
-    :param portal_setup: SetupTool
-    """
-    logger.info("SENAITE LIMS post-install handler [BEGIN]")
-
-    # https://docs.plone.org/develop/addons/components/genericsetup.html#custom-installer-code-setuphandlers-py
-    profile_id = "profile-senaite.lims:default"
-    context = portal_setup._getImportContext(profile_id)
-    portal = context.getSite()  # noqa
-
-    logger.info("SENAITE LIMS post-install handler [DONE]")

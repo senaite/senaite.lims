77784592c0c88e28c0e2428469aa5e040fd05813
Ramon Bartl <rb@ridingbytes.com>
2023-03-10 17:34:26 +0100

    Development is now on 2.5.0

--- src/senaite/lims/upgrades/handlers.py
+++ src/senaite/lims/upgrades/handlers.py
@@ -23,6 +23,18 @@ from senaite.impress import logger
 PROFILE_ID = "profile-senaite.lims:default"
 
 
+def to_2500(portal_setup):
+    """Update to version 2.5.0
+
+    :param portal_setup: The portal_setup tool
+    """
+    logger.info("Run all import steps from SENAITE LIMS ...")
+    context = portal_setup._getImportContext(PROFILE_ID)
+    portal = context.getSite()  # noqa
+    portal_setup.runAllImportStepsFromProfile(PROFILE_ID)
+    logger.info("Run all import steps from SENAITE LIMS [DONE]")
+
+
 def to_2400(portal_setup):
     """Update to version 2.4.0
 
